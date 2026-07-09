3aa9301b45e7068eec7de63ff6c4f84831a9f0aa
ron <6714110003@mut.ac.th>
2026-07-09 17:08:53 +0700

    dev3

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-    echo "Hello Git hub";
-    echo "My name is ronnie";
-    echo "Oh shiiiiiiiii";
-    echo "Holy";
-    echo "Oh asdasdashiiiiiiiii";
-    <br />
-    echo "ooonrimImron";
+    echo "Hello p hub";
 ?>
